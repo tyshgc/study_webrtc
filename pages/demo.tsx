5999447ddde64d6367f93f3da2761c292bf8d1c4
tyshgc <hello@tyshgc.pw>
2020-08-18 16:14:05 +0900

    wip: Added Demo Page.

---
+++ pages/demo.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Head from "next/head";
+
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  );
+}
+
+export default IndexPage;
