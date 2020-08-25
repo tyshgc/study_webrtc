9d0d72e2a1c19d2d6aed31a4beeded092b1836eb
tyshgc <hello@tyshgc.pw>
2020-08-25 16:25:08 +0900

    wip: try Environment Variables UI.

--- pages/api/demo.ts
+++ pages/api/demo.ts
@@ -1,5 +1,6 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+const { channels_app_id: appId } = process.env;
 
 export default (req: NowRequest, res: NowResponse) => {
-  res.json({ name: "John", email: "john@example.com" });
+  res.json({ name: "John", email: "john@example.com", env: appId });
 };
