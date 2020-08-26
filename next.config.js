9809df7ee814af56bf752b7709ad6ad4f055dbf5
tyshgc <hello@tyshgc.pw>
2020-08-26 17:57:41 +0900

    Setup: include .env on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  /* config options here */
+  env: {
+    BRANCH: process.env.BRANCH || process.env.NODE_ENV,
+    PUSHER_APP_ID: process.env.PUSHER_APP_ID,
+    PUSHER_APP_SECRET: process.env.PUSHER_APP_SECRET,
+  },
 };
