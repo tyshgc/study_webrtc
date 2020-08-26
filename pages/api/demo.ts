5feda05bbc76249e9314b06df047ec2e353d9b5d
tyshgc <hello@tyshgc.pw>
2020-08-26 17:54:40 +0900

    Remove: checking Environment Value.

--- pages/api/demo.ts
+++ pages/api/demo.ts
@@ -1,6 +1,5 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-const { channels_app_id: appId } = process.env;
 
 export default (req: NowRequest, res: NowResponse) => {
-  res.json({ name: "John", email: "john@example.com", env: appId });
+  res.json({ name: "John", email: "john@example.com" });
 };
